2196259965f520b5be865c784234031122dca63c
Flammendeingo-85 <Ravi.Banwari1108@gmail.com>
2022-10-09 01:56:24 +0200

    added some comments in JS.file

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// MAIN NAV
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
